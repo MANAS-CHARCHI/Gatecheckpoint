8fe2f21f0fea4ff3e1643ae9f9bf70bb598a1243
Manas Charchi <charchimanas2018@gmail.com>
2024-09-25 13:39:03 +0530

    commite

--- src/Lecturesleft.jsx
+++ src/Lecturesleft.jsx
@@ -8,12 +8,12 @@ export default function(){
                 <div className="mt-8 text-center text-3xl  tracking tracking-wider mb-4">
                     <div className="mb-8"> Lectures Left</div>
                     <div className="flex justify-around mb-8 border-b-2 pb-4">
-                    <div>
+                    {/* <div>
                         <div className="text-xs text-center">Database Management System</div>
                         <div className="text-3xl font-bold text-center mb-3 mt-1"> DBMS</div>
                         <div className="text-center text-base flex justify-between">Query Language: <Dec initialValue={9}/></div>
                         <div className="text-center text-base flex justify-between">File Org. & Indexing: <Dec initialValue={6}/></div>     
-                    </div>
+                    </div> */}
                     <div>
                         <div className="text-xs text-center">Discrete mathematics</div>
                         <div className="text-3xl font-bold text-center mb-3 mt-1"> DM</div>
